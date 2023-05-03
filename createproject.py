eada74c7914f4132babc8ed7fad66450f8f2c8d3
Bibhuti Bhushan Mukhiya <bibhutibhushanmukhiya4086@gmail.com>
2023-05-03 22:07:13 +0545

    convert into exe

--- createproject.py
+++ createproject.py
@@ -1,7 +1,10 @@
 import os
 
-path_of_base_enviroment=input('path of base enviroment : ')
-os.system(os.path.join(f'{path_of_base_enviroment}', 'activate.bat'))
+try:
+    os.system('django-admin --version')
+except:
+    os.system('pip install django')
+
 path=input('enter project path : ')
 project_name=os.path.split(path)
 if project_name[0]=='':
@@ -9,9 +12,6 @@ if project_name[0]=='':
 else:
     os.chdir(f'{project_name[0]}')
     os.system(f"django-admin startproject {project_name[1]}")   
-
-
-
 
 # pip install pyinstaller
 # pyinstaller autocreateproject.py --onefile
